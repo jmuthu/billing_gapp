5a4cc702efbb7075a84db865d16d7fb06902bfa0
Joe Muthu <joemmuthu@gmail.com>
2016-12-14 19:58:38 +0530

    Added ESlint configurationa and vs code formate configuration

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-unused-vars": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
