9ea0ab6efd02330ce59333229d662e20ebe5727b
Joe Muthu <joemmuthu@gmail.com>
2016-12-21 22:01:15 +0530

    Added rollup fpr bundling without modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,42 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 
-gulp.task('build-es6', () =>
+const rollup = require('rollup').rollup;
+const rollupBabel = require('rollup-plugin-babel');
+
+gulp.task('build-es6', function() {
+    return rollup({
+        entry: 'experimental/main/js/com/github/jmuthu/billing/application/BillingService.js',
+        plugins: [
+            rollupBabel({
+                exclude: 'node_modules/**',
+                babelrc: false,
+                presets: ['es2015-rollup'],
+                plugins: ['remove-comments'],
+                generatorOpts: { quotes: 'single' }
+            })
+        ]
+    }).then(function(bundle) {
+        return bundle.write({
+            dest: 'built/billing_gapp_es6.js'
+        });
+    });
+
+});
+
+gulp.task('build-es6-old', () =>
     gulp.src([
         'experimental/**/*/babelHelpers.js',
+        'experimental/**/shared/*.js',
         'experimental/**/model/*.js',
         'experimental/**/spreadSheet/*.js',
         'experimental/**/*/BillingService.js',
         'experimental/**/*/BillingServiceTest.js',
     ])
         .pipe(babel({
-            presets: ['es2015'],
+            presets: [['es2015', {
+                'modules': false
+            }]],
             ignore: ['babelHelpers.js'],
             plugins: ['remove-comments', 'external-helpers', 'transform-remove-strict-mode'],
             generatorOpts: { quotes: 'single' }
