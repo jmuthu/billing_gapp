621a55d71cd71ac95ea0778692fa24a02930db0b
Joe Muthu <joemmuthu@gmail.com>
2017-01-05 14:33:29 +0530

    Ported Billing Service to flow

--- experimental/main/js/com/github/jmuthu/billing/application/BillingService.js
+++ experimental/main/js/com/github/jmuthu/billing/application/BillingService.js
@@ -1,19 +1,28 @@
+// @flow
 import { SubscriberRepositorySpreadsheet } from '../infrastructure/persistence/spreadSheet/SubscriberRepositorySpreadsheet';
 import { BuildingRepositorySpreadsheet } from '../infrastructure/persistence/spreadSheet/BuildingRepositorySpreadsheet';
 import { PricingRepositorySpreadsheet } from '../infrastructure/persistence/spreadSheet/PricingRepositorySpreadsheet';
+import * as SR from '../domain/model/subscriber/SubscriberRepository';
+import * as PR from '../domain/model/pricing/PricingRepository';
+import * as BR from '../domain/model/building/BuildingRepository';
 import { DateUtil } from '../shared/DateUtil';
 import { Exception } from '../shared/Exception';
-class BillingService {
-    constructor(subscriberRepository, buildingRepository, pricingRepository) {
+export class BillingService {
+    subscriberRepository: SR.SubscriberRepository;
+    pricingRepository: PR.PricingRepository;
+    buildingRepository: BR.BuildingRepository;
+    constructor(subscriberRepository: SR.SubscriberRepository,
+        buildingRepository: BR.BuildingRepository,
+        pricingRepository: PR.PricingRepository) {
         // the repo should be done through DI mechanism
         this.subscriberRepository = subscriberRepository;
         this.buildingRepository = buildingRepository;
         this.pricingRepository = pricingRepository;
     }
 
-    runBilling(monthName, year) {
+    runBilling(monthName: string, year: number) {
         let month = DateUtil.getMonthFromString(monthName);
-        Logger.log('Billing started for ' + monthName + ', ' + year);
+        Logger.log(`Billing started for ${monthName}, ${year}`);
         let startDate = new Date(year, month, 1, 0, 0, 0, 0);
         let endDate = new Date(year, month, DateUtil.daysInMonth(month, year), 0, 0, 0, 0);
         let date = new Date();
@@ -28,22 +37,23 @@ class BillingService {
             let subscriberList = this.subscriberRepository.findBillableSubscribers(startDate, endDate);
             let buildingMap = this.buildingRepository.findAll(startDate, endDate);
 
-            for (let subscriberId in subscriberList) {
-                let subscriber = subscriberList[subscriberId];
+            for (let i = 0; i < subscriberList.length; i++) {
+                let subscriber = subscriberList[i];
                 subscriber.setLateFeePricing(this.pricingRepository.find(subscriber.lateFeePricingId));
-
-                for (let subscriptionId in subscriber.subscriptionList) {
-                    let subscription = subscriber.subscriptionList[subscriptionId];
-                    subscription.setPricing(this.pricingRepository.find(subscription.pricingId));
-                    subscription.setBuilding(buildingMap[subscription.buildingId]);
+                if (subscriber.subscriptionList !== undefined) {
+                    for (let j = 0; j < subscriber.subscriptionList.length; j++) {
+                        let subscription = subscriber.subscriptionList[j];
+                        subscription.setPricing(this.pricingRepository.find(subscription.pricingId));
+                        subscription.setBuilding(buildingMap[subscription.buildingId]);
+                    }
                 }
             }
 
-            for (let subscriberId in subscriberList) {
-                subscriberList[subscriberId].runBilling(startDate, endDate);
+            for (let i = 0; i < subscriberList.length; i++) {
+                subscriberList[i].runBilling(startDate, endDate);
             }
             this.subscriberRepository.storeBills(subscriberList, buildingMap, month, year);
-            Logger.log('Billing ended for ' + monthName + ', ' + year);
+            Logger.log(`Billing ended for ${monthName}, ${year}`);
             this.subscriberRepository.releaseLock(lock);
         } catch (exception) {
             Logger.log(exception.message);
